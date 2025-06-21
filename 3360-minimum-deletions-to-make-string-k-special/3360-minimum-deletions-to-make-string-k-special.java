c5c319b95343180dc9319879ca92ec2f08e80e34
Satyam Chaubey <162049915+stym-29@users.noreply.github.com>
2025-06-21 23:55:45 +0530

    Time: 6 ms (51.06%), Space: 45.5 MB (82.27%) - LeetHub

---
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minimumDeletions(String word, int k) {
+        int[] freq = new int[26];
+
+        for(char ch : word.toCharArray()) {
+            freq[ch-'a']++;
+        }
+
+        Arrays.sort(freq);
+
+        int result = Integer.MAX_VALUE;
+        int deletedTillNow = 0;
+
+        for(int i = 0; i < 26; i++) {
+            int minFreq = freq[i];
+            int temp = deletedTillNow;
+
+            for(int j = 25; j > i; j--) {
+                if(freq[j] - freq[i] <= k) 
+                    break;
+                
+                temp += freq[j] - minFreq - k;
+            }
+
+            result = Math.min(result, temp);
+            deletedTillNow += minFreq;
+        }
+
+        return result;
+    }
+}
